0ef528263bf052dcd00006f43f7880c8bfd60410
Paras Mandrah <117353450+Paras-94@users.noreply.github.com>
2023-09-02 12:01:10 +0530

    Add files via upload

---
+++ formfield.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formfield</title>
+</head>
+<body>
+    <p>Formfield</p>
+    <Form>Full Name - Paras Mandrah </Form>
+    <Form>Phone No - 6261497924 </Form>
+    <Form>Email ID  - ParasMandrah@gmail.com </Form>
+    <Form>Subject - PHP  </Form>
+    <Form>Message - Here My code </Form>
+     <formfield name, phone No, Email id, Subject, Message <formfield>
+</body>
+</html>
